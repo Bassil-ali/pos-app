a434c35e9894fbfb30a3cafc5a99615eab5264f5
bassil ali <baselali337@gmail.com>
2020-08-29 05:57:35 -0700

    e.commerce.pos

---
+++ resources/lang/en/site.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    'dashboard' => 'dashboard',
+    'logout' => 'logout',
+
+    'add' => 'add',
+    'create' => 'create',
+    'read' => 'read',
+    'edit' => 'edit',
+    'update' => 'update',
+    'delete' => 'delete',
+    'search' => 'search',
+    'show' => 'show',
+    'loading' => 'loading',
+    'print' => 'print',
+
+    'confirm_delete' => 'confirm_delete',
+    'yes' => 'yes',
+    'no' => 'no',
+
+    'login' => 'login',
+    'remember_me' =>'remember_me',
+    'password' => 'password',
+    'password_confirmation' =>'password_confirmation',
+
+    'added_successfully' => 'added_successfully',
+    'updated_successfully' => 'updated_successfully',
+    'deleted_successfully' =>  'deleted_successfully',
+
+    'no_data_found' => 'no_data_found',
+    'no_records' => 'no_records',
+
+    'clients' => 'clients',
+    'client_name' =>  'client_name',
+    'phone' => 'phone',
+    'address' =>  'address',
+    'previous_orders' => 'previous_orders',
+    'orders' => 'orders',
+    'add_order' =>  'add_order',
+    'edit_order' =>  'edit_order',
+
+    'users' => 'users',
+    'first_name' => 'first_name',
+    'last_name' => 'last_name',
+    'email' =>  'email',
+    'image' =>  'image',
+    'action' => 'action',
+
+    'permissions' =>'permissions',
+
+    'categories' => 'categories',
+    'all_categories' => 'all_categories',
+    'name' => 'name',
+    'description' => 'description',
+    'products_count' => 'products_count',
+    'related_products' => 'related_products',
+    'category' => 'category',
+    'show_products' => 'show_products',
+    'created_at' => 'created_at',
+
+    'products' => 'products',
+    'product' => 'product',
+    'quantity' => 'quantity',
+    'total' => 'total',
+    'purchase_price' => 'purchase_price',
+    'price' => 'price',
+    'sale_price' => 'sale_price',
+    'stock' => 'stock',
+    'profit_percent' => 'profit_percent',
+
+    'ar' => [
+        'name' => 'name for english',
+        'description' => 'description for english',
+    ],
+
+    'en' => [
+        'name' => 'name',
+        'description' =>'description',
+    ],
+
+];
